1423fd70b28f05dabcd5af36d36be404eec8b1e4
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-12-24 20:14:48 +0500

    added new change

---
+++ index.js
@@ -0,0 +1,42 @@
+//1----Easy:
+let arr = [1, 2, 3, 4, 5];
+arr.reverse();
+console.log(arr);
+
+//2----
+let str = "hello";
+console.log(str.toUpperCase());
+//3----
+let n = [1, 2, 3];
+ n.push(4);
+console.log(n);
+//4----
+let len = [7, 8, 9];
+console.log(len.length);
+
+//5----
+let me = "Hello world";
+let newone = me.replace("world", "Javascript")
+console.log(newone);
+
+//6----
+let one = [4, 5, 6];
+let a  =one.shift();
+console.log(a);
+
+//7----
+let uz = "Uzbekistan";
+console.log(uz.length);
+
+//8----
+let massiv = [2, 3, 4];
+massiv.unshift(5);
+console.log(massiv);
+
+//9----
+let small = "JAVASCRIPTDA";
+console.log(small.toLowerCase());
+//10----
+let fruits = ["Apple", "Banana", "Cherry"];
+
+
